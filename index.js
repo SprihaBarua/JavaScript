a0f57c8c29ccb85c9fe8442032e3e39f6dcbeb84
SprihaBarua <isprihabarua@gmail.com>
2024-09-02 16:50:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+/*
+var name = "UCTC";
+
+console.log("Hello ", + name);
+*/
+
+/*
+let id = 1;
+console.log(id);
+*/
+
+/*
+const pi = 3.1416;
+console.log(pi);
+*/
+
+/*
+var x = 10;
+
+function testVar()
+{
+    var y = 20;
+    console.log(x);
+}
+
+console.log(y);
+*/
+
+/*
+let x = 10;
+
+function testLet()
+{
+    let y = 20;
+    console.log(x);
+    console.log(y);
+}
+
+console.log(y);
+*/
+
+/*
+const pi = 3.1416;
+
+pi = 3.14;
+console.log(pi);
+*/
+
+/*
+//hoisting
+console.log(a);  //Var can be hoist
+var a = 5;
+console.log(a);
+*/
+
+/*
+console.log(a);  //let can't be hoist
+let a = 5;
+console.log(a);
+*/
+
+/*
+function sayhi(name)
+{
+    console.log("Hello " + name);
+}
+sayhi("UCTC")
+*/
+
+/*
+const sayhi = (name) => 
+{
+    console.log("Hello", name);
+}
+
+sayhi("UCTC")
+*/
+
+/*
+// Template Litreals (back-ticks) [` `]
+const name = "UCTC";
+console.log(`Hello ${name}`)
+*/
+
+/*
+//Array Destructuring
+const friends = ["Rakib", "Sakib", "Sabbir" ];
+const [First, Second, Third] = friends;
+
+console.log(First, Second, Third);
+*/
+
+/*
+//object Destructuring
+const person = {
+    name: "Rakib",
+    age: 23
+}
+
+const {name, age} = person;
+console.log(name, age);
+*/
---
+++ main.js
@@ -0,0 +1,5 @@
+import { add } from "./math";
+import { subtract } from "./math";
+
+console.log(add(497, 50));
+console.log(subtract(497, 50));
